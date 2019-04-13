d1010b7c4c1fc48021a92e8ba65d682a9c4b90c6
qinero <1151664257@qq.com>
2019-04-13 17:16:50 +0800

    连接数据库

---
+++ src/com/weibo/util/DButil.java
@@ -0,0 +1,20 @@
+package com.weibo.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DButil {
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConn() throws Exception {
+        Connection conn;
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/weibo", "root", "201610204032");
+        return conn;
+    }
+}
