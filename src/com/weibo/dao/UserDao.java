f176086163a3b1956c0e7e0ff7e89ec50aa48bd5
qinero <1151664257@qq.com>
2019-04-13 18:32:51 +0800

    根据用户ID查询用户的方法

---
+++ src/com/weibo/dao/UserDao.java
@@ -0,0 +1,43 @@
+package com.weibo.dao;
+
+import com.weibo.pojo.User;
+import com.weibo.util.DButil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+    public User selectName(String user) {
+        try {
+            Connection conn = DButil.getConn();
+            PreparedStatement search = conn.prepareStatement("SELECT * FROM user WHERE userId=?");
+            search.setString(1, user);
+            ResultSet result = search.executeQuery(); // 执行
+            User users = null;
+
+            while (result.next()) {
+                users = new User();
+                users.setUserId(result.getInt("userId"));
+                users.setUserName(result.getString("userName"));
+                users.setUserPassword(result.getString("userPassword"));
+                users.setFans(result.getInt("fans"));
+                users.setCarePeople(result.getInt("carePeople"));
+                users.setMicroblog(result.getInt("microblog"));
+                users.setChatHead(result.getString("chatHead"));
+                users.setAphorism(result.getString("aphorism"));
+            }
+            result.close();
+            search.close();
+            conn.close();
+            return users;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
