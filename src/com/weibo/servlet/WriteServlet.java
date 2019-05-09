594881c8dc8e55a1273b689310bea0753ef5a261
qinero <1151664257@qq.com>
2019-05-09 19:52:37 +0800

    写微博服务

---
+++ src/com/weibo/servlet/WriteServlet.java
@@ -0,0 +1,27 @@
+package com.weibo.servlet;
+
+import com.weibo.dao.UserDao;
+import com.weibo.pojo.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "WriteServlet", urlPatterns = {"/WriteServlet"})
+public class WriteServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+
+        String  content= request.getParameter("content");
+
+    }
+}
+
