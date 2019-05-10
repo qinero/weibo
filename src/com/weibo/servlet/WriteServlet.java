8e6474bf6becfdd469dff5024996754beedfcd2a
qinero <1151664257@qq.com>
2019-05-10 21:35:11 +0800

    写微博逻辑层

--- src/com/weibo/servlet/WriteServlet.java
+++ src/com/weibo/servlet/WriteServlet.java
@@ -1,7 +1,7 @@
 package com.weibo.servlet;
 
-import com.weibo.dao.UserDao;
-import com.weibo.pojo.User;
+import com.weibo.util.Fileutil;
+import com.weibo.util.Ziputil;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,6 +21,16 @@ public class WriteServlet extends HttpServlet {
         response.setCharacterEncoding("utf-8");
 
         String  content= request.getParameter("content");
+
+        Ziputil zip=new Ziputil();
+        Fileutil save=new Fileutil();
+        byte[] compressed;
+        String decompressed;
+
+        compressed=zip.compress(content); // 将文字压缩
+        save.saveFile(compressed);
+
+        decompressed=zip.decompress(compressed); // 得到解压后的文字
 
     }
 }
