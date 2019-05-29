2b996c23857c75f9025ed02eed1e462d2de2136a
qinero <1151664257@qq.com>
2019-05-29 15:23:00 +0800

    登录逻辑层

--- src/com/weibo/servlet/LoginServlet.java
+++ src/com/weibo/servlet/LoginServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
@@ -28,13 +29,18 @@ public class LoginServlet extends HttpServlet {
         user = ud.selectName(userName);
 
         if (user == null) {
+            // 创建新用户
 
         } else {
-            if (pwd.equals(user.getUserPassword()))
+            if (pwd.equals(user.getUserPassword())) {
+                int id=user.getUserId();
+                HttpSession session = request.getSession();
+                session.setAttribute("user", id);
+                // request.setAttribute("user", user);
+                //System.out.println("正确");
                 request.getRequestDispatcher("home.jsp").forward(request, response);
 
-            //   System.out.println("正确");
+            }
         }
-
     }
 }
