907562f96096902d69eab643319a9c20197b1332
qinero <1151664257@qq.com>
2019-06-03 21:16:44 +0800

    登录逻辑层

--- src/com/weibo/servlet/LoginServlet.java
+++ src/com/weibo/servlet/LoginServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
@@ -28,19 +29,21 @@ public class LoginServlet extends HttpServlet {
 
         User user = ud.selectName(userName);
 
-        if (user == null) {
-            // 创建新用户
-            request.getRequestDispatcher("sign_in.jsp").forward(request, response);
-        } else {
+        if (user != null) {
             if (pwd.equals(user.getUserPassword())) {
-                int id=user.getUserId();
+                // session 保存用户名
+                String username =user.getUserName();
                 HttpSession session = request.getSession();
-                session.setAttribute("user", id);
-                // request.setAttribute("user", user);
-                //System.out.println("正确");
+                session.setAttribute("username", username);
                 request.getRequestDispatcher("home.jsp").forward(request, response);
-
             }
+        } else {
+            // 创建新用户
+            // 提示注册窗口
+            response.setContentType("text/html;charset=utf-8");
+            PrintWriter out=response.getWriter();
+            out.print("<script language='javascript'>('请先注册')");
+            out.print("<script>window.location.href='sign_in.jsp'</script>");
         }
     }
 }
